6245d93cb9c92cc5b26b4e9ad6ac34a74c5ec055
Linzp <sunandmoon001@163.com>
2026-03-06 13:32:16 +0800

    再修改一下

--- src/components/TalentProfile/FormInner/PromotionHistoryFormInner.js
+++ src/components/TalentProfile/FormInner/PromotionHistoryFormInner.js
@@ -11,7 +11,7 @@ const PromotionHistoryFormInner = createWithRemoteLoader({
     <TableList
       name="promotionHistory"
       title="晋升历史"
-      list={[<DatePicker name="time" label="晋升时间" picker="month" format="YYYY-MM" rule="REQ" />, <Input name="occupation" label="岗位名称" rule="REQ LEN-0-100" />, <Input name="level" label="晋升等级" rule="REQ LEN-0-100" />]}
+      list={[<DatePicker name="time" label="晋升时间" picker="month" format="YYYY-MM" rule="REQ" />, <Input name="occupation" label="岗位名称" rule="REQ LEN-0-100" />, <Input name="level" label="晋升等级" rule="LEN-0-100" />]}
     />
   );
 });
