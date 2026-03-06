1c82d62a64226b6de46ae854a08b2e6853158939
Linzp <sunandmoon001@163.com>
2026-03-06 09:20:38 +0800

    提交，发布一版

--- src/preset.js
+++ src/preset.js
@@ -72,7 +72,7 @@ export const globalInit = async () => {
     //url: 'http://localhost:3001',
     //tpl: '{{url}}',
     remote: 'components-core',
-    defaultVersion: '0.4.63'
+    defaultVersion: '0.4.64'
   };
   remoteLoaderPreset({
     remotes: {
