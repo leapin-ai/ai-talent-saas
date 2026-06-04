7286dc6d54524b0fc19ccf80c8801020f78ad4aa
Linzp <sunandmoon001@163.com>
2026-06-04 15:46:16 +0800

    提交一下

--- src/components/TenantUserPlugin/locale/en-US.js
+++ src/components/TenantUserPlugin/locale/en-US.js
@@ -1,7 +1,10 @@
 const enUS = {
   'tenantUser.position': 'Position',
   'tenantUser.joinDate': 'Join Date',
-  'tenantUser.workStartDate': 'Work Start Date'
+  'tenantUser.workStartDate': 'Work Start Date',
+  'tenantUser.addPosition': 'Add Position',
+  'tenantUser.addPositionFailed': 'Failed to add position',
+  'tenantUser.positionName': 'Position Name'
 };
 
 export default enUS;
